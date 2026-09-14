ec780caeb0ea5c6da6b5c3718c1db8c35692e0f4
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-15 02:07:55 +0530

    refactor(core): export research evidence graph

--- src/core/index.ts
+++ src/core/index.ts
@@ -9,6 +9,7 @@ export * from './astro/validation-matrix';
 export * from './astro/invariants';
 export * from './astro/observer';
 export * from './astro/topocentric';
+export * from './research/evidence';
 export * from './time/scales';
 export * from './panchanga/ayanamsa';
 export * from './panchanga/rules';
