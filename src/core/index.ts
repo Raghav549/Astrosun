fbc5b89a54af7ff3e9ed7c0c2baeedb34d989023
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-15 02:09:58 +0530

    refactor(core): export dynamics invariants

--- src/core/index.ts
+++ src/core/index.ts
@@ -14,6 +14,7 @@ export * from './astro/rise-set';
 export * from './dynamics/state';
 export * from './dynamics/kepler';
 export * from './dynamics/nbody';
+export * from './dynamics/invariants';
 export * from './physics/constants';
 export * from './research/evidence';
 export * from './time/scales';
