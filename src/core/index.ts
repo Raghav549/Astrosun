094c4298af32c086b1a4adbaf6dac2bb91a4efec
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-15 02:09:30 +0530

    refactor(core): export N-body dynamics primitives

--- src/core/index.ts
+++ src/core/index.ts
@@ -13,6 +13,7 @@ export * from './astro/horizons';
 export * from './astro/rise-set';
 export * from './dynamics/state';
 export * from './dynamics/kepler';
+export * from './dynamics/nbody';
 export * from './research/evidence';
 export * from './time/scales';
 export * from './panchanga/ayanamsa';
